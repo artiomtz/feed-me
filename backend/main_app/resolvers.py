02d508b2167fbdd41bbe5f2116af41e71045f44a
Artiom Tsimkin <artiomtz@gmail.com>
2023-10-13 16:57:58 -0400

    feat: add recipe suggestions logic

--- backend/main_app/resolvers.py
+++ backend/main_app/resolvers.py
@@ -1,8 +1,52 @@
 from data_import.models import Recipe
+from decouple import config
+import re
 
 
-def resolve_recipe_by_id(root, info, recipe_id):
+RECIPE_SIMILARITY_THRESHOLD = float(config("RECIPE_SIMILARITY_THRESHOLD"))
+MAX_INGREDIENTS = int(config("MAX_INGREDIENTS"))
+MIN_INGREDIENTS = int(config("MIN_INGREDIENTS"))
+
+
+def resolve_recipe_by_id(self, info, recipe_id):
     try:
         return Recipe.objects.get(pk=recipe_id)
     except Recipe.DoesNotExist:
         return None
+
+
+def clean_ingredient(ingredient):
+    return re.sub(r"\[|\]|\'", "", ingredient).strip()
+
+
+def resolve_possible_recipes(self, info, ingredients):
+    cleaned_ingredients = {clean_ingredient(ingredient) for ingredient in ingredients}
+
+    if (
+        len(cleaned_ingredients) < MIN_INGREDIENTS
+        or len(cleaned_ingredients) > MAX_INGREDIENTS
+    ):
+        print(
+            f"Please provide between {MIN_INGREDIENTS} to {MAX_INGREDIENTS} ingredients"
+        )
+        return []
+
+    filtered_recipes = []
+    all_recipes = Recipe.objects.all()
+
+    for recipe in all_recipes:
+        recipe_ingredients = [
+            clean_ingredient(ingredient)
+            for ingredient in recipe.ingredients_set.split(", ")
+        ]
+
+        similarity_score = len(
+            cleaned_ingredients.intersection(recipe_ingredients)
+        ) / len(recipe_ingredients)
+
+        if similarity_score >= RECIPE_SIMILARITY_THRESHOLD:
+            filtered_recipes.append(recipe)
+    print(
+        f"Found {len(filtered_recipes)} matching recipes out of {len(all_recipes)} recipes"
+    )
+    return filtered_recipes
--- backend/main_app/schema.py
+++ backend/main_app/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django.types import DjangoObjectType
 from data_import.models import Recipe
-from .resolvers import resolve_recipe_by_id
+from .resolvers import resolve_recipe_by_id, resolve_possible_recipes
 
 
 class RecipeType(DjangoObjectType):
@@ -13,12 +13,18 @@ class RecipeType(DjangoObjectType):
 class Query(graphene.ObjectType):
     all_recipes = graphene.List(RecipeType)
     recipe_by_id = graphene.Field(RecipeType, recipe_id=graphene.Int())
+    possible_recipes = graphene.List(
+        RecipeType, ingredients=graphene.List(graphene.String)
+    )
 
     def resolve_all_recipes(self, info):
         return Recipe.objects.all()
 
     def resolve_recipe_by_id(self, info, recipe_id):
         return resolve_recipe_by_id(self, info, recipe_id)
+
+    def resolve_possible_recipes(self, info, ingredients):
+        return resolve_possible_recipes(self, info, ingredients)
 
 
 schema = graphene.Schema(query=Query)
