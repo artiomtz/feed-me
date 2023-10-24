469b0435f1928212c2c694be457c1f6d7d115320
Artiom Tsimkin <artiomtz@gmail.com>
2023-10-24 14:08:41 -0400

    feat: add filtered ingredients json option

--- backend/scripts/get_ingredients_set.py
+++ backend/scripts/get_ingredients_set.py
@@ -47,4 +47,15 @@ indexed_json = json.dumps(indexed_ingredients, indent=2)
 with open(config("LOCAL_ALL_INGREDIENTS_EXPORT_JSON"), "w") as json_output_file:
     json_output_file.write(indexed_json)
 
+indexed_ingredients_filtered = {char: [] for char in "abcdefghijklmnopqrstuvwxyz"}
+
+for ingredient in unique_ingredients_filtered:
+    indexed_ingredients_filtered[ingredient[0]].append(ingredient)
+
+indexed_json_filtered = json.dumps(indexed_ingredients_filtered, indent=2)
+
+with open(config("LOCAL_INGREDIENTS_EXPORT_JSON"), "w") as json_output_file_filtered:
+    json_output_file_filtered.write(indexed_json_filtered)
+
 print(f"Found {len(unique_ingredients)} unique ingredients.")
+print(f"Filtered down to {len(unique_ingredients_filtered)} unique ingredients.")
