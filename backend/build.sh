68f776656469d631ae027b63c8748f3b312f1ba0
Artiom Tsimkin <artiomtz@gmail.com>
2023-10-24 20:37:49 -0400

    feat: add build script

---
+++ backend/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py makemigrations
+python manage.py migrate
+
+python manage.py data_import/management/commands/download_and_import_csv
