7387209131b9b6409d48821726d36854c5741014
Artiom Tsimkin <artiomtz@gmail.com>
2023-10-30 13:12:37 -0400

    fix: build script

--- backend/build.sh
+++ backend/build.sh
@@ -4,6 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
+mkdir -p /opt/render/project/src/backend/staticfiles/
+
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
