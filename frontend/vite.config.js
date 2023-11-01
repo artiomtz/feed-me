7bd959fee40a7b45b2c7b607c07b56ccc5308fce
Artiom Tsimkin <artiomtz@gmail.com>
2023-11-01 18:43:30 -0400

    fix: enable graphql

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
+import resolve from "@rollup/plugin-node-resolve";
 
 dotenv.config();
 const isProduction = process.env.NODE_ENV === "production";
@@ -17,5 +18,10 @@ export default defineConfig({
     MAX_INGREDIENTS: `"${process.env.MAX_INGREDIENTS}"`,
     MIN_INGREDIENTS: `"${process.env.MIN_INGREDIENTS}"`,
   },
-  plugins: [react()],
+  plugins: [react(), resolve()],
+  build: {
+    rollupOptions: {
+      external: ["graphql"],
+    },
+  },
 });
